2346c392717b1c5055454133222c07e21ab4c50a
Michael Botes <michaelbotes21@gmail.com>
2018-12-11 08:31:20 +0200

    started with db integration on prisma, struggling with local global package

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const {GraphQLServer} = require('graphql-yoga');
+const { Prisma } = require('prisma-binding')
 
 
 let links  = [{
@@ -12,48 +13,35 @@ let  idCount = links.length
 const resolvers = {
     Query: {
         info: () => 'null',
-        feed: () => links,
+        feed: (root, args, context, info) => {
+            return context.db.query.links({}, info)
+        },
         link: (props) => links[props.id] 
     },
     Mutation: {
-        post: (root, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url
-            }
-            links.push(link)
-            return link
-        },
-
-        updateLink: (root, args) => {
-            const link = {
-                id: args.id,
-                description: args.description,
-                url: args.url
-            }
-
-            var foundIndex = links.findIndex(x => x.id== link.id)
-            links[foundIndex] = link
-            return link
-
-        },
-
-        deleteLink: (root, args) => {
-            var foundIndex = links.findIndex(x => x.id == args.id)
-
-            if (foundIndex !== -1) {
-                links.splice(foundIndex, 1)
-            }
-
-            return {data: "delted!"}
-        }
+       post: (root, args, context, info) => {
+           return context.db.Mutation.createLink({
+               data: {
+                   url: args.url,
+                   description: args.description
+               }
+           }, info)
+       }
     }
 }
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers
+    resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+          typeDefs: 'src/generated/prisma.graphql',
+          endpoint: 'https://eu1.prisma.sh/michael-botes-934aac/database/dev',
+          secret: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InNlcnZpY2UiOiJkYXRhYmFzZUBkZXYiLCJyb2xlcyI6WyJhZG1pbiJdfSwiaWF0IjoxNTQ0NDQ1MjU3LCJleHAiOjE1NDUwNTAwNTd9.pXa9UczmsVzrd7f8Q93IS0-h7_222ySqQLaOIiZDQww',
+          debug: true,
+        }),
+      }),
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
